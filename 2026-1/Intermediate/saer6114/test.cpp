864446636292770a44ad634af8bc34f2926d4aad
saer6114 <saer6114@naver.com>
2026-03-19 22:01:16 +0900

    .

---
+++ 2026-1/Intermediate/saer6114/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string a;
+
+    cin >> a;
+    cout << a << endl;
+
+    cout << "Hello World!" << endl;
+
+    cout << "ABCDDEDF";
+
+    return 0;
+}
