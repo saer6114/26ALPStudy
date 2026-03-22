b386868d69e82b1e9076af8eb083d9c99b389060
saer6114 <saer6114@naver.com>
2026-03-23 01:11:46 +0900

    .

---
+++ 2026-1/Intermediate/saer6114/DP/BOJ_11055.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define max(a,b) (a > b) ? a : b
+ 
+int N;
+int arr[1001];
+int dp[1001];
+ 
+int main(void)
+{
+    cin >> N;
+ 
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+ 
+    for (int i = 0; i < N; i++)
+    {
+        dp[i] = arr[i];
+        for (int j = 0; j < i; j++)
+        {
+            // 증가하는 수열일 때, 합을 갱신한다.
+            if (arr[j] < arr[i]) 
+                dp[i] = max(dp[i], dp[j] + arr[i]);
+        }
+    }
+ 
+    int ans = dp[0];
+    
+    for (int i = 1; i < N; i++)
+        if (ans < dp[i]) 
+            ans = dp[i];
+ 
+    cout << ans;
+ 
+    return 0;
+}
